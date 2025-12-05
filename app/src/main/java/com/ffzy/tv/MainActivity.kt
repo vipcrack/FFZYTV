073bc2a4a8e9f0dc090a50f792a7c8b41c0d30ab
vipcrack <bcgcrack@163.com>
2025-12-05 14:34:49 +0800

    Add files via upload

---
+++ app/src/main/java/com/ffzy/tv/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ffzy.tv
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContentView(R.layout.activity_main)
+    }
+}
