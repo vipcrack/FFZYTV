b894b137fc493a18679278b42e573c66aaf325f7
vipcrack <bcgcrack@163.com>
2025-12-05 12:02:41 +0800

    Add files via upload

--- app/src/main/java/com/ffzy/tv/MainActivity.java
+++ app/src/main/java/com/ffzy/tv/MainActivity.java
@@ -1,18 +1,40 @@
 package com.ffzy.tv;
+
 import android.os.Bundle;
-import android.webkit.WebView;
+import android.view.KeyEvent;
 import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
-    @Override protected void onCreate(Bundle savedInstanceState) {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        WebView wv = new WebView(this);
-        setContentView(wv);
-        WebSettings ws = wv.getSettings();
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings ws = webView.getSettings();
         ws.setJavaScriptEnabled(true);
         ws.setDomStorageEnabled(true);
+        ws.setDatabaseEnabled(true);
+        ws.setCacheMode(WebSettings.LOAD_DEFAULT);
+        ws.setAllowFileAccess(true);
         ws.setUseWideViewPort(true);
         ws.setLoadWithOverviewMode(true);
-        wv.loadUrl("http://cj.ffzyapi.com/");
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://cj.ffzyapi.com/");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
