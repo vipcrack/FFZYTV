b2ecf151162c5d86ca2ddbdd30e7acd4a81d7846
vipcrack <bcgcrack@163.com>
2025-12-05 10:28:41 +0800

    Add files via upload

---
+++ app/src/main/java/com/ffzy/tv/MainActivity.java
@@ -0,0 +1,12 @@
+package com.ffzy.tv;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 空界面，仅用于通过编译
+    }
+}
